7e70b77ace90cc5c84d524cb2ea809bdf04a2778
Quotz <andrej.kocevski.koc@gmail.com>
2022-12-05 11:14:06 -0500

    calculator working properly

--- script.js
+++ script.js
@@ -16,6 +16,8 @@ function multiply(a,b) {
 
 function operate(a,operator,b) {
     let result = 0;
+    a = Number(a);
+    b = Number(b);
 
     switch (operator) {
 
@@ -37,4 +39,128 @@ function operate(a,operator,b) {
     }
 
     return result;
-}
+};
+
+function setDisplay(x) {
+    let display = document.querySelector('#display');
+    display.textContent = x
+};
+
+function clear() {
+    firstOperand = 0;
+    secondOperand = 0;
+    operator = undefined;
+    storedOperand = "";
+    setDisplay(storedOperand);
+    console.log("cleared");
+};
+
+let firstOperand;
+let secondOperand;
+let operator;
+let storedOperand;
+
+clear();
+
+let numberList = document.querySelectorAll(".operand");
+for (const number of numberList) {
+    number.addEventListener('click', function(){
+        let x = number.textContent;
+
+        if (firstOperand!=undefined && operator!=undefined) {
+            storedOperand += x;
+            setDisplay(storedOperand);
+            secondOperand = storedOperand;
+            console.log(`Second operand set: ${secondOperand}`);    
+
+        } else {
+        storedOperand += x;
+        setDisplay(storedOperand);
+        firstOperand = storedOperand;
+        console.log(`First Operand set: ${firstOperand}`);
+        };
+    });
+};
+
+let operatorList = document.querySelectorAll(".operator");
+for (const i of operatorList) {
+    i.addEventListener('click', function() {
+        let x = i.textContent;
+
+        if (x == "C") {
+            clear();
+            setDisplay(storedOperand);
+
+        } else if (x == "=") {
+            if (firstOperand && secondOperand && operator){
+                let result = operate(firstOperand, operator, secondOperand);
+                setDisplay(result);
+                firstOperand = result;
+                secondOperand = 0;
+                operator = undefined;
+                storedOperand = "";
+            } else {
+                clear()
+                setDisplay("Error");
+            };
+
+        } else if (firstOperand && x!="=") {
+            if (secondOperand && operator) {
+                let result = operate(firstOperand, operator, secondOperand);
+                setDisplay(result);
+                firstOperand = result;
+                operator = x;
+                secondOperand = 0;
+                storedOperand = "";
+            } else {
+                operator = x;
+                storedOperand = "";
+            };
+
+        };
+    });
+};
+
+
+//         // } else if (operator && secondOperand) {
+//         //     let result = operate(firstOperand, operator, secondOperand);
+//         //     setDisplay(result);
+//         //     firstOperand = result;
+//         //     secondOperand = undefined;
+//         //     storedOperand = "";
+
+//         //     if (x == "=") {
+//         //         operator = "";
+//         //         console.log ("operator =, no next operator")
+
+//         //     } else {
+//         //     operator = x;
+//         //     console.log(`next operator ${x}`);
+//         //     };
+
+//         // } else {
+//         // operator = x;
+//         // firstOperand = Number(storedOperand);
+//         // storedOperand = "";
+//         // };
+//     });
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
